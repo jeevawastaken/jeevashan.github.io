db70851dee6a77f3c0977ae17380566fd5e5892c
jeevawastaken <144551578+jeevawastaken@users.noreply.github.com>
2024-03-28 15:04:21 +0530

    cgs

--- fresh-start/freshy-Mcfresh/src/app/app.module.ts
+++ fresh-start/freshy-Mcfresh/src/app/app.module.ts
@@ -3,12 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppRingPlacementComponent } from './app-ring-placement/app-ring-placement.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    AppRingPlacementComponent
   ],
   imports: [
     BrowserModule,
